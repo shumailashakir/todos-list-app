84ea50cc7315f7d0496ebd8debff2a50cfb8cb74
shumaila shakir <160516126+shumailashakir@users.noreply.github.com>
2024-04-19 23:07:59 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+let fruitchaat = ["banana", "apple", "orange"];
+// console.log(fruitchaat)
+fruitchaat.push("melon");
+// console.log(fruitchaat)
+fruitchaat.pop();
+// console.log(fruitchaat)
+fruitchaat.concat([]);
+fruitchaat = fruitchaat.concat(["melon", "kiwi"]);
+console.log(fruitchaat);
+export {};
---
+++ main.ts
@@ -0,0 +1,14 @@
+import inquirer from "inquirer"
+let fruitchaat = ["banana","apple","orange"] 
+// console.log(fruitchaat)
+
+fruitchaat.push("melon")
+// console.log(fruitchaat)
+
+fruitchaat.pop()
+// console.log(fruitchaat)
+
+fruitchaat.concat([])
+
+fruitchaat = fruitchaat.concat(["melon","kiwi"]) 
+console.log(fruitchaat)
